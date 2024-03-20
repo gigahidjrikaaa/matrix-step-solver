e7748245389c190809e6efc3ce9caba66dc8cfdb
salwamhrn11 <salwamaharani920@gmail.com>
2024-03-20 12:09:08 +0700

    nama

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def AboutPage():
     st.divider()
     st.write('This project is made by:')
     st.write('1. Giga Hidjrika Aura Adkhy - 21/479228/TK/52833')
-    st.write('2. ')
+    st.write('2. Salwa Maharani - 21/481194/TK/53113')
     st.write('3. ')    
 
 
