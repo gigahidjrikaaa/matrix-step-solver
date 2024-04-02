6e67525c628e01a82236c2439184f1724855aed8
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-04-02 10:27:09 +0700

    fix: dataframe to latex

--- app.py
+++ app.py
@@ -3,6 +3,22 @@ import pandas as pd
 import numpy as np
 from fractions import Fraction
 
+######################
+# Remove the column index
+######################
+def remove_column_index(latex_matrix):
+    # Find the \toprule index
+    top_rule_index = latex_matrix.find(r'\toprule')
+    # Find the \midrule index
+    mid_rule_index = latex_matrix.find(r'\midrule')
+    # Remove the string between \toprule and \midrule
+    latex_matrix = latex_matrix[:top_rule_index] + latex_matrix[mid_rule_index + 8:]
+    # Find the \bottomrule index
+    bottom_rule_index = latex_matrix.find(r'\bottomrule')
+    # Remove the second character before \bottomrule until \bottomrule
+    latex_matrix = latex_matrix[:bottom_rule_index - 2] + latex_matrix[bottom_rule_index + 11:]
+    return latex_matrix
+    
 
 ######################
 # Function for matrix number 1
@@ -10,7 +26,8 @@ from fractions import Fraction
 def display_matrix(matrix):
     df = pd.DataFrame(matrix)
     # Convert the dataframe to KaTeX format
-    latex_matrix = df.to_latex(index=False, escape=False)
+    latex_matrix = df.to_latex(index=False, escape=False,)
+    latex_matrix = remove_column_index(latex_matrix)
     # Round the numbers to 2 decimal places
     latex_matrix = latex_matrix.replace('.000000', '').replace('.000', '').replace('.00', '')
     latex_matrix = latex_matrix.replace(r'\begin{tabular}', r'\begin{bmatrix}').replace(r'\end{tabular}', r'\end{bmatrix}'.replace(r'\hline', '')).replace(r'\\', r'\\ \,').replace(r'\n', r'').replace(r'\toprule', '').replace(r'\midrule', '').replace(r'\bottomrule', '').replace(r'rrrr', r'')
